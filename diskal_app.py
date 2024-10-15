bb95550934d86097f37b9510bccf293b59b1c86d
Naama Dayan <namadayan@gmail.com>
2024-10-15 22:09:40 +0300

    change

--- diskal_app.py
+++ diskal_app.py
@@ -16,5 +16,4 @@ app.layout = html.Div(children=[
 ])
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 8050))
-    app.run_server(host="0.0.0.0", port=port)
+    app.run_server()
